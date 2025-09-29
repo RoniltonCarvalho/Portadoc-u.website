965ccaaef0bd7055ce10d8d58cc2af215f52af3c
RoniltonCarvalho <oesteengseg@gmail.com>
2025-09-28 22:57:29 -0300

    Update liturgia.js

--- api/liturgia.js
+++ api/liturgia.js
@@ -2,9 +2,14 @@ export default async function handler(req, res) {
   try {
     const rssUrl = "https://www.evangelizo.org/rss/evangelho.xml";
     const resposta = await fetch(rssUrl);
+
+    if (!resposta.ok) {
+      throw new Error("Erro ao buscar RSS");
+    }
+
     const texto = await resposta.text();
 
-    // Retorna o XML com CORS liberado
+    // Retorna como XML com CORS liberado
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "application/xml; charset=utf-8");
     res.status(200).send(texto);
