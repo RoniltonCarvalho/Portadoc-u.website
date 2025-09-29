14a9d4802196f93c308106150c9258dcf15dc947
RoniltonCarvalho <oesteengseg@gmail.com>
2025-09-28 22:51:58 -0300

    Create liturgia.js

---
+++ api/liturgia.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  try {
+    const rssUrl = "https://www.evangelizo.org/rss/evangelho.xml";
+    const resposta = await fetch(rssUrl);
+    const texto = await resposta.text();
+
+    // Retorna o XML com CORS liberado
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", "application/xml; charset=utf-8");
+    res.status(200).send(texto);
+  } catch (err) {
+    res.status(500).json({ erro: "Falha ao carregar liturgia", detalhe: err.message });
+  }
+}
